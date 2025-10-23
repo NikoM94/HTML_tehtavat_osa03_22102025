0204169db73cae36c6656282dad401c085a52230
niko.muukkonen <niko.muukkonne@edu.keuda.fi>
2025-10-23 08:36:41 +0300

    tehtävä 6 tehty

--- src/t5/t5.js
+++ src/t5/t5.js
@@ -1,9 +1,9 @@
 function fizzBuzz() {
     for (let i = 1; i <= 100; i++) {
-        document.getElementById("t5v").innerText += (i % 3 === 0 && i % 5 === 0) ? "hipheijaa" :
+        document.getElementById("t5v").innerHTML += (i % 3 === 0 && i % 5 === 0) ? "hipheijaa" :
             (i % 3 === 0) ? "hip" :
             (i % 5 === 0) ? "heijaa" :
             i;
-        document.getElementById("t5v").innerText += "\n";
+        document.getElementById("t5v").innerHTML += "\n";
     }
 }
